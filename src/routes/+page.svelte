78655dd5b0af885c2dc5e67d3912877ee9e3ec39
Miha Majetič <mihamajetic@users.noreply.github.com>
2025-01-29 13:51:18 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let x = 0;
+
+	function onClick() {
+		x = x + 1;
+	}
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<button class="p-2 bg-blue-500 text-white rounded-lg" on:click={onClick}>
+	Kliknil si me {x}-krat
+</button>
